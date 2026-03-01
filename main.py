b13a7b70053cfc878cd4f1133232b69d0777b048
Splayer0 <mynamesplayer@gmail.com>
2026-03-01 20:26:39 +0530

    fixed some minor stuff and added comments for a roadmap on implementing more functions regarding formatting

--- main.py
+++ main.py
@@ -217,18 +217,30 @@ def launch_gui_with_usb_data() -> None:
 
 ### DISK FORMATTING ###
 def volumecustomlabel():
+    # 1. detect the file type
+    # 2. unmount the drive
+    # 3. change the label using the command specific for that file type
+    # 4. mount the drive again
     pass
 
 def cluster():
+    # detect the current selected cluster and put it in a variable
+    # convert the clusters to sectors fat32
     pass
 
 def quickformat():
+    # detect quick format option ticked or not and put it in a variable
+    # the if logic will be implemented later
     pass
 
 def createextended():
+    # detect create extended label and icon files check box and put it in a variable
     pass
 
 def checkdevicebadblock():
+    # following may be used?
+    # pkexec badblocks -wsv path
+    # no idea how to use passes tho
     pass
 
 def dskformat(usb_mount_path):
@@ -249,7 +261,7 @@ def dskformat(usb_mount_path):
     # instead of FileNotFoundError we can also use shutil(?)
     if type==0:
         try:
-            subprocess.run(["pkexec", "mkfs.ntfs", "-Q", path])
+            subprocess.run(["pkexec", "mkfs.ntfs", "-c", clusters, "-Q", path])
             print("success format to ntfs!")
         except FileNotFoundError:
             pkexecNotFound()
@@ -259,7 +271,7 @@ def dskformat(usb_mount_path):
             unexpected()
     elif type==1:
         try:
-            subprocess.run(["pkexec", "mkfs.vfat", "-F", "32", path])
+            subprocess.run(["pkexec", "mkfs.vfat", "s", sectors, "-F", "32", path], check=True)
             print("success format to fat32!")
         except FileNotFoundError:
             pkexecNotFound()
@@ -269,7 +281,7 @@ def dskformat(usb_mount_path):
             unexpected()
     elif type==2:
         try:
-            subprocess.run(["pkexec", "mkfs.exfat", path])
+            subprocess.run(["pkexec", "mkfs.exfat", "-b", clusters, path], check=True)
             print("success format to exFAT!")
         except FileNotFoundError:
             pkexecNotFound()
@@ -279,7 +291,7 @@ def dskformat(usb_mount_path):
             unexpected()
     elif type==3:
         try:
-            subprocess.run(["pkexec", "mkfs.ext4", {path}])
+            subprocess.run(["pkexec", "mkfs.ext4", "-b", clusters, path], check=True)
             print("success format to ext4!")
         except FileNotFoundError:
             pkexecNotFound()
@@ -287,6 +299,8 @@ def dskformat(usb_mount_path):
             FormatFail()
         except Exception:
             unexpected()
+    else:
+        print("FS TYPE IS INVALID?")
 
     
 
