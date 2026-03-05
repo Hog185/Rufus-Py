fe4cf66fc1f79a0e5d546a59a80ac75ab8c3a8eb
Hog <hogman12333@gmail.com>
2026-03-05 19:49:37 +1100

    move away from numbers for image options etc

--- src/rufus_py/drives/states.py
+++ src/rufus_py/drives/states.py
@@ -1,7 +1,7 @@
 ##### THIS FILE HOLDS ALL THE STATES OF THE OPTION CHOSEN BY THE USER #####
-
-currentFS = 0
-image_option = 0
+partition_scheme=""
+image_option=""
+currentFS = ""
 partition_scheme = 0
 target_system = 0
 cluster_size = 0
@@ -9,4 +9,3 @@ QF = 0
 create_extended = 0
 check_bad = 0
 new_label = "Volume Label"
-iso_path: str = ""
